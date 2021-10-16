79de30d76c1063c9e795040ec7c4595fd968b0ef
emadjumaah <emadjumaah@gmail.com>
2021-10-16 18:10:18 +0300

    before copy from electron

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,15 +1,20 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { isElectron } from "../common";
 
 const stringstore = localStorage.getItem("store");
 const store = stringstore ? JSON.parse(stringstore) : null;
 
 const token = store ? store.token : null;
 
-// local
+const uri = isElectron
+  ? "https://jadwal-web.herokuapp.com/"
+  : process?.env?.GRAPHQL_URI
+  ? process.env.GRAPHQL_URI
+  : "http://jadwal-main:4000/graphql";
+
 const httpLink = createHttpLink({
-  uri: "http://jadwal-main:4000/graphql",
-  // uri: process.env.GRAPHQL_URI,
+  uri,
 });
 
 const authLink = setContext((_, { headers }) => {
