bb3a4a4cf4559f35c6c2bb2dabb02646d14c87a6
emadjumaah <emadjumaah@gmail.com>
2021-11-02 05:05:27 +0300

    uui graphql

--- src/App.tsx
+++ src/App.tsx
@@ -43,12 +43,12 @@ function App() {
   //   : 'http://jadwal-main:4000/graphql';
 
   // const uri = 'https://jadwal-prod.herokuapp.com/graphql'; // desktop test
-  const uri = 'https://jadwal-prod.herokuapp.com/graphql'; // desktop prod and vercel
+  // const uri = 'https://jadwal-prod.herokuapp.com/graphql'; // desktop prod and vercel
   // const uri = 'http://jadwal-main:4000/graphql'; // localserver
   // const uri = process.env.GRAPHQL_URI; // webserver
 
   const httpLink = createHttpLink({
-    uri,
+    uri: 'https://jadwal-prod.herokuapp.com/graphql',
   });
 
   const authLink = setContext((_, { headers }) => {
