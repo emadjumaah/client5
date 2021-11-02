e660d4e1e22e7e3f19ab69b5969154c8d293c7d3
emadjumaah <emadjumaah@gmail.com>
2021-11-02 20:01:22 +0300

    some code in main

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,7 @@ function App() {
   // const uri = 'http://jadwal-main:4000/graphql'; // localserver
   // const uri = process.env.GRAPHQL_URI; // webserver
   // env react
+  // some code
   const httpLink = createHttpLink({
     uri: process.env.REACT_APP_GRAPHQL_URI,
   });
