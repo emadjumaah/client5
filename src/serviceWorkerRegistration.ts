e20a7480b96459a52fcedf044faa613303a4c5a7
emadjumaah <emadjumaah@gmail.com>
2021-11-18 10:53:21 +0300

    test sw second time

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -22,7 +22,7 @@ const isLocalhost = Boolean(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
-
+// test after first time
 type Config = {
   onSuccess?: (registration: ServiceWorkerRegistration) => void;
   onUpdate?: (registration: ServiceWorkerRegistration) => void;
